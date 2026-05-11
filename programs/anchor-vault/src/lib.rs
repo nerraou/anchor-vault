7c06fb27540df114e76b4a1b29065faaf83ba333
nerraou <nouhaylaerraou@gmail.com>
2026-05-11 21:42:22 +0100

    feat: create initialize instruction

--- programs/anchor-vault/src/lib.rs
+++ programs/anchor-vault/src/lib.rs
@@ -16,6 +16,6 @@ pub mod anchor_vault {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+        ctx.accounts.initialize(&ctx.bumps)
     }
 }
